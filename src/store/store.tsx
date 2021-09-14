4558ba32bb7a788dee3b5f6b5b95848135a37623
naustica <nick.haupka@outlook.de>
2021-09-14 18:12:08 +0200

    add header, footer, etc...

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,12 +1,4 @@
 import createStore, { Store } from 'unistore'
-// @ts-ignore
-import DataFrame from 'dataframe-js'
-
-const df = new DataFrame({
-  bundesland: ["DE-BW", "DE-BW", "DE-MV", "DE-MV", "DE-MV", "DE-HB", "DE-NW", "DE-MV", "DE-ST", "DE-TH"],
-  oa_policy: [true, false, false, true, true, true, false, false, true, true],
-  oa_leitlinie: [true, false, true, true, true, true, false, true, true, true]
-}, ['bundesland', 'oa_policy', "oa_leitlinie"])
 
 export interface StoreState {
   df: any,
@@ -14,7 +6,7 @@ export interface StoreState {
 }
 
 export const initialState: StoreState = {
-  df: df,
+  df: null,
   filter: ["bundesland"]
 }
 
